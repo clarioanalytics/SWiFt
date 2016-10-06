c4b79654bf2bc174549947de92e9a600f80052c5
George Coller <george@coller.com>
2016-10-06 12:22:21 -0500

    Issue #12 - Including other decisions along with a FailWorkflowExecution decision can mess up proper workflow completion ---- - added check to retain only the last-added FailWorkflowExecution decision if one exists in the decisions list. -- any removed decisions are logged - added a warning log if zero decisions are made during a poll, which could indicate a workflow log bug that will cause a workflow to hang.

--- src/test/java/com/clario/swift/DecisionPollerTest.java
+++ src/test/java/com/clario/swift/DecisionPollerTest.java
@@ -1,14 +1,18 @@
 package com.clario.swift;
 
+import com.amazonaws.services.simpleworkflow.model.Decision;
+import com.amazonaws.services.simpleworkflow.model.DecisionType;
 import com.clario.swift.examples.workflows.TimerWorkflow;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author George Coller
  */
 public class DecisionPollerTest {
-
 
     @Test
     public void testAddDuplicateWorkflowInstance() {
@@ -24,5 +28,32 @@ public class DecisionPollerTest {
             return;
         }
         Assert.fail("Expecting exception");
+    }
+
+    @Test
+    public void testCheckFailWorkflowExecutionDecisionExists() {
+        DecisionPoller poller = new DecisionPoller("poller", "domain", "taskList", "context");
+        List<Decision> decisions = new ArrayList<>();
+        Decision failWorkflowDecision = new Decision().withDecisionType(DecisionType.FailWorkflowExecution);
+        decisions.add(new Decision().withDecisionType(DecisionType.FailWorkflowExecution));
+        decisions.add(new Decision().withDecisionType(DecisionType.StartTimer));
+        decisions.add(failWorkflowDecision);
+
+        poller.checkFailWorkflowExecutionDecision(decisions);
+        Assert.assertEquals("Expect exactly one decision", 1, decisions.size());
+        Assert.assertEquals("Expect last-added fail decision", failWorkflowDecision, decisions.get(0));
+    }
+
+    @Test
+    public void testCheckFailWorkflowExecutionDecisionNotExists() {
+        DecisionPoller poller = new DecisionPoller("poller", "domain", "taskList", "context");
+        List<Decision> decisions = new ArrayList<>();
+        decisions.add(new Decision().withDecisionType(DecisionType.StartTimer));
+        decisions.add(new Decision().withDecisionType(DecisionType.RecordMarker));
+        decisions.add(new Decision().withDecisionType(DecisionType.ScheduleActivityTask));
+        List<Decision> expected = new ArrayList<>(decisions);
+
+        poller.checkFailWorkflowExecutionDecision(decisions);
+        Assert.assertEquals("Expect no change in decision list", expected, decisions);
     }
 }
